890ad9c6ad42ff25a659f4e0b70d1aac0f6eeaf2
FanOfStars <ramazanovapea@gmail.com>
2025-05-26 19:02:00 +0300

    изменение под бд (без таблицы токена)

--- src/main/kotlin/register/RegisterRouting.kt
+++ src/main/kotlin/register/RegisterRouting.kt
@@ -1,16 +1,8 @@
 package ru.fanofstars.register
 
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.request.receive
-import io.ktor.server.response.respond
-import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
-import ru.fanofstars.cache.InMemoryCache
-import ru.fanofstars.cache.TokenCache
-import ru.fanofstars.utils.isValidEmail
-import java.util.UUID
 
 fun Application.configureRegisterRouting() {
 
